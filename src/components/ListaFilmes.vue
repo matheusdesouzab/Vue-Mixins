6c757ec7e0c236729a72aeef9775752af8dad765
Matheus de Souza <matheusdesouza187@gmail.com>
2022-12-16 23:21:55 -0300

    first commit

---
+++ src/components/ListaFilmes.vue
@@ -0,0 +1,19 @@
+<template>
+    <h3>Lista de Filmes</h3>
+    <input type="text" placeholder="Nome do jogo" v-model="item">
+    <button type="button" @click="adicionarItem()">Adicionar</button>
+    <ul>
+        <li v-for="(item, index) in items" :key="index">{{ item }}</li>
+    </ul>
+    <button type="button" @click="removerItem">Remover filme</button>
+</template>
+
+<script>
+import ListaMixin from '@/mixins/ListaMixin';
+import RemoverItem from '@/mixins/RemoverItem';
+
+export default {
+    name: 'ListaFilmes',
+    mixins: [ListaMixin, RemoverItem]
+}
+</script>
---
+++ src/components/ListaPessoas.vue
@@ -0,0 +1,20 @@
+<template>
+    <h3>Lista Pessoas</h3>
+    <input type="text" placeholder="Nome da pessoa" v-model="item">
+    <button type="button" @click="adicionarItem()">Adicionar</button>
+    <ul>
+        <li v-for="(item, index) in items" :key="index">{{ item }}</li>
+    </ul>
+    <button type="button" @click="removerItem()">Remover pessoa</button>
+</template>
+
+
+<script>
+import ListaMixin from '@/mixins/ListaMixin';
+import RemoverItem from '@/mixins/RemoverItem';
+
+export default{
+    name: 'ListaPessoas',
+    mixins: [ListaMixin, RemoverItem]
+}
+</script>
